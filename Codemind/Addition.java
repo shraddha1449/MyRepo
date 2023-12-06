f969061fcb01562e2192c95a358073b7a0967d41
shraddha1449 <81550610+shraddha1449@users.noreply.github.com>
2023-12-06 16:30:41 +0530

    Add files via upload

---
+++ Codemind/Addition.java
@@ -0,0 +1,15 @@
+
+class Addition{
+
+public static void main(String args[]){
+	
+	int a = 10;
+	int b = 20;
+	
+	int res = a+b;
+	
+	System.out.println("Addition of two number is - "+res);
+
+}
+
+}
